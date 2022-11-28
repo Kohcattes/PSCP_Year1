636f0e3b70de5fbbfecd49f8697c88a64fbc2a3d
65070247 Atikun Pidsayo <109953754+aomati145@users.noreply.github.com>
2022-11-28 18:06:27 +0700

    Add files via upload

--- Capture.py
+++ Capture.py
@@ -1,9 +1,9 @@
 import cv2  
 cam_port = 0
-cam = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 total, dic_pic = 1, dict()
 while True:
-    result, image = cam.read()
+    result, image = camera.read()
     cv2.imshow("photo", image)
     if cv2.waitKey(1) & 0x0FF == ord("q"):
         cv2.imwrite("photo%d.png" %total, image)
@@ -13,3 +13,4 @@ while True:
         break
 print(dic_pic)
 cv2.destroyWindow()
+
