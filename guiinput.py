93931a4bca1e9a627f2e7c2a50fac143f59de579
SupakornThongaerd <118120963+SupakornThongaerd@users.noreply.github.com>
2022-12-10 22:12:54 +0700

    Add files via upload

--- guiinput.py
+++ guiinput.py
@@ -1,23 +1,31 @@
 from tkinter import *
+import cv2  
 
-win = Tk()
+win=Tk()
+win.title('Hello Python')
+win.option_add("*Font", "Helvetica")
 win.geometry("1000x550")
+lbl1=Label(win, text='Number Of Rows                         :').place(x=100, y=50)
+lbl2=Label(win, text='Number Of People Per Rows    :').place(x=100, y=100)
+lbl3=Label(win, text='Photo To Be Taken                     :').place(x=100, y=200)
+Button(win, text='Take Photo', width=20, bd=3, command=win.quit).place(x=650, y=100)
 
-Label(win, text="Rows:").grid(row=0)       
-Label(win, text="Columns:").grid(row=1)
+t1=Entry(win, bd=3)
+t2=Entry(win, bd=3)
+t3=Entry(bd=4)
+t1.place(x=350, y=50)
+t2.place(x=350, y=100)
+t3.place(x=350, y=200)
 
-e1 = Entry(win)
-e2 = Entry(win)
-e1.grid(row=0, column=1, padx=10, pady=5)
-e2.grid(row=1, column=1, padx=10, pady=5)
-
-def show():
-    print(e1.get())
-    print(e2.get())
-    e1.delete(0, "end")
-    e2.delete(0, "end")
-
-Button(win, text="Get Info", width=10, command=show).grid(row=3, column=0, sticky= "w", padx=10, pady=5)
-Button(win, text="Exit", width=10, command=win.quit).grid(row=3, column=1, sticky="e", padx=10, pady=5)
-
+def add():
+    print(t1.get())
+    print(t2.get())
+    ans = (int(t1.get()))*(int(t2.get()))
+    t3.insert(END, str(ans))
+    
+# def takephoto():
+    #ใส่ฟังค์ชั่นcapture
+    
+    
+Button(win, text='Enter', width=20, bd=1, command=add).place(x=200, y=150)
 win.mainloop()
