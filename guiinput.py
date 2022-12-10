d2ec84632fc65b5986c7cfb3ff333d8c718a4c11
SupakornThongaerd <118120963+SupakornThongaerd@users.noreply.github.com>
2022-12-10 23:34:35 +0700

    Add files via upload

--- guiinput.py
+++ guiinput.py
@@ -1,21 +1,22 @@
 from tkinter import *
 import cv2  
+import customtkinter
 
-win=Tk()
-win.title('Hello Python')
+win= customtkinter.CTk()
+win.title('ทุกระบบจบที่บัง')
 win.option_add("*Font", "Helvetica")
 win.geometry("1000x550")
-lbl1=Label(win, text='Number Of Rows                         :').place(x=100, y=50)
-lbl2=Label(win, text='Number Of People Per Rows    :').place(x=100, y=100)
-lbl3=Label(win, text='Photo To Be Taken                     :').place(x=100, y=200)
-Button(win, text='Take Photo', width=20, bd=3, command=win.quit).place(x=650, y=100)
+customtkinter.set_appearance_mode("drak")
+lbl1=customtkinter.CTkLabel(win, text='Number Of Rows                         :').place(x=100, y=35)
+lbl2=customtkinter.CTkLabel(win, text='Number Of People Per Rows    :').place(x=100, y=75)
+lbl3=customtkinter.CTkLabel(win, text='Photo To Be Taken                     :').place(x=100, y=155)
 
-t1=Entry(win, bd=3)
-t2=Entry(win, bd=3)
-t3=Entry(bd=4)
-t1.place(x=350, y=50)
-t2.place(x=350, y=100)
-t3.place(x=350, y=200)
+t1=customtkinter.CTkEntry(win, placeholder_text="number")
+t2=customtkinter.CTkEntry(win, placeholder_text="number")
+t3=customtkinter.CTkEntry(win, placeholder_text="result")
+t1.place(x=350, y=35)
+t2.place(x=350, y=75)
+t3.place(x=350, y=155)
 
 def add():
     print(t1.get())
@@ -23,9 +24,19 @@ def add():
     ans = (int(t1.get()))*(int(t2.get()))
     t3.insert(END, str(ans))
     
-# def takephoto():
-    #ใส่ฟังค์ชั่นcapture
+def takephoto():
+    cam = cv2.VideoCapture(1)
+    total = 1
+    while True:
+        result, image = cam.read()
+        cv2.imshow("photo", image)
+        if cv2.waitKey(1) & 0x0FF == ord("q"):
+            cv2.imwrite("photo%d.png" %total, image)
+            total += 1
+        if cv2.waitKey(1) & 0x0FF == ord("d"):
+            break
+    cv2.destroyWindow()
     
-    
-Button(win, text='Enter', width=20, bd=1, command=add).place(x=200, y=150)
+customtkinter.CTkButton(win, text='Take Photo', width=150, command=takephoto).place(x=650, y=100)
+customtkinter.CTkButton(win, text='Enter', width=100, command=add).place(x=200, y=120)
 win.mainloop()
