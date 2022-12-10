43a234b7f3c7bb8771d6f753ccc5358af8c50ab6
65070247 Atikun Pidsayo <109953754+aomati145@users.noreply.github.com>
2022-12-10 16:37:27 +0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,21 +1,31 @@
 import cv2
+import tkinter import *
 
 cam_port = 0
 cam = cv2.VideoCapture(0)
-total, dic_pic = 1, dict()
+dic_pic = dict()
 #เพิ่มส่วนของการหาตำแหน่ง
-
+face_track = cv2.CscadeClassifier('face_alt.xml')
 #end of position
-while True:
-    result, image = cam.read()
-    cv2.imshow("photo", image)
-    if cv2.waitKey(1) & 0x0FF == ord("q"):
-        cv2.imwrite("photo%d.png" %total, image)
-        dic_pic['photo'+str(total)+'.png'] = total
-        total += 1
-    if cv2.waitKey(1) & 0x0FF == ord("d"):
-        break
-print(dic_pic)
+def opencamera():
+    total = 1
+    while True:
+        result, image = cam.read()
+        cv2.imshow("input student len", image)
+        if cv2.waitKey(1) & 0x0FF == ord("q"):
+            cv2.imwrite("photo%02d.png" %total, image)
+            img = cv2.imread("photo%02d.png" %total)
+            gray_scal = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            y = (face_track.detectMultiScale(gray_scal))[0][1]
+            dic_pic['photo%02d.png'%total] = y
+            total += 1
+        if cv2.waitKey(1) & 0x0FF == ord("d"):
+            break
+    chair = tuple(sorted(dic_pic.items(), key=lambda x:x[1]))
+    return chair
+base = Tk()
+base.title("โปรแกรมหาที่นั่ง")
+
 cv2.destroyWindow()#สักคนแก้ตรงนี้ดิ้ไม่ให้มันError ตอนปิดหน้าต่าง เอมแก้ให้หน่อย
 #ได้รูปนักศึกษามาแล้วต่อไปเป็นส่วนของการจัดเรียง
 
@@ -29,6 +39,4 @@ human = body_model.detectMultiScale(gray_scal) #save
 print(human) # keep to show
 for x, y, w, h in human:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2) #แก้ไงดีวะกลัวว่ามันจะแท็ค หลายจุดแล้วloopแตก
-cv2.imshow('Aom', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
