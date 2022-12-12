64df0818e9fd9492a4d7a05326726b94e9e64a01
65070188Mananchaya <112749019+65070188Mananchaya@users.noreply.github.com>
2022-12-12 22:37:46 +0700

    Add files via upload

--- gui_output.py
+++ gui_output.py
@@ -1,19 +1,24 @@
 from tkinter import *
 import customtkinter
 
-customtkinter.set_appearance_mode("lilght")
+customtkinter.set_appearance_mode("light")
 table = customtkinter.CTk()
 table.title("ทุกระบบจบที่บัง")
 table.geometry("1000x550")
 
-inp_row, inp_column = 7, 4
+inp_row, inp_column = 4, 14
+total = 0
 f1,f2 = Frame(table), Frame(table)
 f1.grid(row=0, column=0, rowspan=2)
 f2.grid(row=2, column=0)
-Label(f1, text="Classroom", width=25, bg="#CBAACB").pack(padx=385, pady=10)
-ans = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22"]
-# for x in ans:
-for x in ans:
-    Button(f2, text=x).grid(padx=5, pady=5)
+Label(f1, text="Classroom", width=110, bg="#CBAACB").pack(padx=1, pady=10)
+ans = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+for i in range(inp_row):
+    for j in range(inp_column):
+        if total > len(ans)-1:
+            Button(f2, text="").grid(row=i, column=j)
+        else:
+            Button(f2, text=ans[total]).grid(row=i, column=j)
+            total += 1
 
 table.mainloop()
